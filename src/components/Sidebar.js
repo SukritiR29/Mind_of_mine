b3376f518bed0330e5d3ff9509a52fb132f76543
Sukriti <87890532+SukritiR29@users.noreply.github.com>
2023-03-21 19:20:09 +0530

    sidebar update

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -14,11 +14,12 @@ import {
 } from "@heroicons/react/outline";
 
 function Sidebar() {
-  return <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full">
+  return (
+  <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full">
     <div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24">
     <Image src="/main_logo.png" width={40} height={40} />
     </div>
-    <div className="space-y-2.5 mt-4 mb-2.5 xl:ml-24">
+    <div className="mt-4 mb-2.5 xl:ml-20 space-y-">
         <SidebarLink text="Home" Icon={HomeIcon} active />
         <SidebarLink text="Explore" Icon={HashtagIcon} />
         <SidebarLink text="Notifications" Icon={BellIcon} />
@@ -29,7 +30,7 @@ function Sidebar() {
         <SidebarLink text="More" Icon={DotsCircleHorizontalIcon} />
     </div>
   </div>
-  
+  );
 }
 
 export default Sidebar
