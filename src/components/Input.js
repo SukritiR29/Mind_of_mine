047dcf951f693f81233e40e970bd1f40fd3b9ac7
Sukriti <87890532+SukritiR29@users.noreply.github.com>
2023-03-23 22:17:35 +0530

    input update

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
 function Input() {
+    const [input, setInput] = useState()
   return (
-    <div>
+    <div className={'border-b border-gray-700 p-3 flex space-x-3 overflow-y-scroll'}>
       <img src="dp.png" alt="" className="h-9 w-9 rounded-full cursor-pointer " />
+      
+      <div className='w-full divide-y divide-gray-700'>
+        <div className={''}>
+        <textarea value={input} id="" cols="30" rows="10"></textarea>
+        </div>
+        
+      </div>
+      
     </div>
   )
 }
